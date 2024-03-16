622b0eafb4ba413b6d15240af28af48a13387acc
Iafcy622 <frankiecheung512@gmail.com>
2024-03-16 22:38:10 +0800

    Updated heap

--- heap/minHeap.c
+++ heap/minHeap.c
@@ -1,30 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Heap
+typedef struct _Heap
 {
     int *arr;
     int size;
     int capacity;
-};
+} Heap;
 
-typedef struct Heap heap;
+Heap *createHeapLinear(int capacity, int *nums);
+Heap *createHeap(int capacity);
+void insertHelper(Heap *h, int index);
+void minHeapify(Heap *h, int index);
+int extractMin(Heap *h);
+void insert(Heap *h, int data);
+void freeHeap(Heap *h);
 
-heap *createHeap(int capacity);
-void insertHelper(heap *h, int index);
-void heapify(heap *h, int index);
-int extractMin(heap *h);
-void insert(heap *h, int data);
-
-heap *createHeap(int capacity)
+Heap *createHeapLinear(int capacity, int *nums) // O(n)
 {
-    heap *h = (heap *)malloc(sizeof(heap));
+    Heap *h = (Heap *)malloc(sizeof(Heap));
 
     if (h == NULL)
-    {
-        printf("Memory error");
         return NULL;
-    }
 
     h->size = 0;
     h->capacity = capacity;
@@ -32,32 +29,53 @@ heap *createHeap(int capacity)
     h->arr = (int *)malloc(capacity * sizeof(int));
 
     if (h->arr == NULL)
-    {
-        printf("Memory error");
         return NULL;
-    }
+
+    int i;
+    for (i = 0; i < capacity; i++)
+        h->arr[i] = nums[i];
+
+    h->size = i;
+    i = (h->size - 2) / 2; // parent of last element
+    while (i >= 0)
+        minHeapify(h, i--);
 
     return h;
 }
 
-// helper to maintain minHeap property
-void insertHelper(heap *h, int index)
+Heap *createHeap(int capacity)
+{
+    Heap *h = (Heap *)malloc(sizeof(Heap));
+
+    if (h == NULL)
+        return NULL;
+
+    h->size = 0;
+    h->capacity = capacity;
+
+    h->arr = (int *)malloc(capacity * sizeof(int));
+
+    if (h->arr == NULL)
+        return NULL;
+
+    return h;
+}
+
+void insertHelper(Heap *h, int index)
 {
     int parent = (index - 1) / 2;
 
     if (h->arr[parent] > h->arr[index])
     {
-        // swap arr[parent] and arr[index]
         int temp = h->arr[parent];
         h->arr[parent] = h->arr[index];
         h->arr[index] = temp;
 
-        // recursively check the above level
         insertHelper(h, parent);
     }
 }
 
-void heapify(heap *h, int index)
+void minHeapify(Heap *h, int index)
 {
     int left = index * 2 + 1;
     int right = index * 2 + 2;
@@ -66,7 +84,6 @@ void heapify(heap *h, int index)
     left = (left >= h->size || left < 0) ? -1 : left;
     right = (right >= h->size || right < 0) ? -1 : right;
 
-    // set min to the index of the min between arr[left], arr[right], arr[index]
     if (left != -1 && h->arr[left] < h->arr[index])
         min = left;
     if (right != -1 && h->arr[right] < h->arr[min])
@@ -74,35 +91,31 @@ void heapify(heap *h, int index)
 
     if (min != index)
     {
-        // swap arr[min], arr[index]
         int temp = h->arr[min];
         h->arr[min] = h->arr[index];
         h->arr[index] = temp;
 
-        // recursively check the lower level
-        heapify(h, min);
+        minHeapify(h, min);
     }
 }
 
-int extractMin(heap *h)
+int extractMin(Heap *h)
 {
     int min;
 
-    // empty heap
     if (h->size == 0)
         return -999;
 
     min = h->arr[0];
 
-    // swap root with last element
     h->arr[0] = h->arr[h->size - 1];
     h->size--;
 
-    heapify(h, 0);
+    minHeapify(h, 0);
     return min;
 }
 
-void insert(heap *h, int data)
+void insert(Heap *h, int data)
 {
     if (h->size < h->capacity)
     {
@@ -112,21 +125,28 @@ void insert(heap *h, int data)
     }
 }
 
-void printHeap(heap *h)
+void printHeap(Heap *h)
 {
 
     for (int i = 0; i < h->size; i++)
-    {
         printf("%d ", h->arr[i]);
-    }
+
     printf("\n");
+}
+
+void freeHeap(Heap *h)
+{
+    if (h == NULL)
+        return;
+    free(h->arr);
+    free(h);
 }
 
 int main()
 {
     int arr[9] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
     int n = sizeof(arr) / sizeof(int);
-    heap *hp = createHeap(n);
+    Heap *hp = createHeap(n);
 
     for (int i = 0; i < n; i++)
     {
@@ -134,6 +154,7 @@ int main()
     }
 
     printHeap(hp);
+    freeHeap(hp);
 
     return 0;
 }
